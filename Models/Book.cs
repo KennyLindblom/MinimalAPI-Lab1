a4116839f5cc2e19122cd8033528271c082e4b2e
Kenny Lindblom <65187682+KennyLindblom@users.noreply.github.com>
2023-09-24 21:32:16 +0200

    Add files via upload

---
+++ Models/Book.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace NewLab1_MinimalAPI.Models
+{
+    public class Book
+    {
+
+        [Key]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string Author { get; set; }
+
+        public string Genre { get; set; }
+
+        public bool IsAvaliabel { get; set; }
+
+        public int ReleaseYear { get; set; }
+
+        public string Description { get; set; }
+    }
+}
